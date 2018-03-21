f1b082a24533b70eac6ca082af1f471867403e10
Uy Pham <uy@vencreative.com.au>
2018-03-21 21:55:11 +0700

    [UPDATE] CORE

--- src/components/button-blue/index.jsx
+++ src/components/button-blue/index.jsx
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 // import "./style.css";
 
 class ButtonGreen extends Component {
-    onYouClick(){
-        alert("Yeah!");
-        // console.log(this);
-    }
     render() {
         return (
-            <button onClick={()=>this.onYouClick()} style={{color: "#fff", background: "blue"}}>{this.props.children}</button>
+            <button style={{ color: "#fff", background: "blue" }}>{this.props.children}</button>
         );
     }
 }
--- src/home.jsx
+++ src/home.jsx
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import ButtonGreen from "coms/button-blue";
 
+const data = [
+    { name: "Nguyễn Văn A", grade: 6 },
+    { name: "Trần Văn Dũng", grade: 2 },
+    { name: "Nguyễn Văn A", grade: 3 },
+    { name: "Nguyễn Văn GF", grade: 6 },
+    { name: "Nguyễn Văn F", grade: 6 },
+]
+
 class Homepage extends Component {
-    componentWillMount(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            lophoc: data
+        }
+    }
+
+
+    componentWillMount() {
         document.title = "Homepage";
+    }
+    liClick(hs) {
+        alert(hs.name + " - Lớp: " + hs.grade);
+    }
+    addHocSinh() {
+        this.setState(prevState => ({
+            lophoc: [...prevState.lophoc, { name: "Nguyễn Văn Thêm", grade: 7 }]
+        }));
+    }
+    onChangeTen(e) {
+        // console.log(this/.refs.inputTen);
+        // this.refs.txtTen.textContent = this.refs.inputTen.value;
+        this.setState({ textTen: this.refs.inputTen.value });
     }
     render() {
         return (
             <div>
                 <p>Homepage is here :)</p>
-                <p><ButtonGreen>CLICK ME</ButtonGreen></p>
+                <p onClick={() => this.addHocSinh()}><ButtonGreen  >CLICK ME</ButtonGreen></p>
+                <h4>Danh sách lớp học</h4>
+                <ul>
+                    {this.state.lophoc.map((hocsinh, i) => {
+                        return (
+                            <li onClick={() => this.liClick(hocsinh)} key={i}>
+                                {hocsinh.name}
+                                {hocsinh.grade > 5 && `- Lớp: ${hocsinh.grade}`}
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                <h1>FORM</h1>
+                <input onChange={e => this.onChangeTen(e)} ref="inputTen" placeholder="Nhập tên..." />
+                <p ref="txtTen">{this.state.textTen}</p>
             </div>
         );
     }
